639f826805fad62ca229d44ca8456f951d5bd1ef
Dong Zeng <zeng-dong@hotmail.com>
2022-05-21 15:05:42 -0500

    refactoring extract function

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ function App() {
 
   const books = [{ name: "Refactoring"}, { name: "Domain-driven design"}];
 
+  const renderBooks = (books) => {
+    return <div data-test='book-list'>
+      {
+        books.map(book => (
+          <div className="book-item">
+            <h2 className="title">{book.name}</h2>
+          </div>
+        ))
+      }
+    </div>;
+  }
+
   return (
     <div className="App">
       <Typography variant="h2" component='h2' data-test='heading'>Bookish</Typography>
     
-      <div data-test='book-list'>
-        {
-          books.map(book => (
-            <div className="book-item">
-              <h2 className="title">{book.name}</h2>
-            </div>  
-            ))
-        }
-      </div>    
+      {renderBooks(books)}    
     </div>    
   );
 }
