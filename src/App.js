db860dc102421868664383df1eb2d184307f07b1
Dong Zeng <zeng-dong@hotmail.com>
2022-05-21 15:00:32 -0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import { Typography } from '@mui/material';
 
 function App() {
+
+  const books = [{ name: "Refactoring"}, { name: "Domain-driven design"}];
+
   return (
     <div className="App">
       <Typography variant="h2" component='h2' data-test='heading'>Bookish</Typography>
     
       <div data-test='book-list'>
-        <div className='book-item'>
-          <h2 className='title'>Refactoring</h2>          
-        </div>
-        <div className='book-item'>
-          <h2 className='title'>Domain-driven design</h2>
-        </div>
+        {
+          books.map(book => (
+            <div className="book-item">
+              <h2 className="title">{book.name}</h2>
+            </div>  
+            ))
+        }
       </div>    
     </div>    
   );
