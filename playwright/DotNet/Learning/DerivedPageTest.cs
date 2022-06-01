3c8b1799b9231c5a9880754a15d5cfa12033cd13
Dong Zeng <zeng-dong@hotmail.com>
2022-05-31 19:28:52 -0500

    use Expect

--- playwright/DotNet/Learning/DerivedPageTest.cs
+++ playwright/DotNet/Learning/DerivedPageTest.cs
@@ -11,16 +11,23 @@ namespace DotNet.Learning;
 [Category("learning")]
 public class DerivedPageTest : PageTest
 {
+    [SetUp]
+    public async Task Setup()
+    {
+        await Page.GotoAsync("http://www.eaapp.somee.com");
+    }
+
     [Test]
     public async Task Derived_page_test_goes_to_app()
     {
-        await Page.GotoAsync("http://www.eaapp.somee.com");
         await Page.ClickAsync("text=Login");
-
         await Page.FillAsync("#UserName", "admin");
         await Page.FillAsync("#Password", "password");
         await Page.ClickAsync("text=Log in");
-        var exists = await Page.Locator("text='Employee Details'").IsVisibleAsync();
-        Assert.IsTrue(exists);
+
+        ///var exists = await Page.Locator("text='Employee Details'").IsVisibleAsync();
+        ///Assert.IsTrue(exists);
+
+        await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
     }
 }
